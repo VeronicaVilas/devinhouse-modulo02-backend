bb96a1aeaef6270a344d824a4ff3044980d76be5
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-07-26 18:59:12 -0300

    style: style naming adjustment

--- index.php
+++ index.php
@@ -8,46 +8,43 @@
     <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-    
-<h1 class="main-title">- Algoritmos -</h1>
+    <h1 class="main-title">- Algoritmos -</h1>
     <h3 class="main-subtitle">Módulo 02: Semana 01</h3>
-
     <section class="algorithms-list">
-        
         <a href="pages/calculadora-de-imc/index.php">
             <div class="algorithms-item">
-                <div class="notiglow"></div>
-                <div class="notiborderglow"></div>
-                <div class="notititle">Calculadora de IMC</div>
-                <div class="notibody">Calcula o Índice de Massa Corporal com base na altura e peso fornecidos.</div>
+                <div class="algorithms-item-glow"></div>
+                <div class="algorithms-item-borderglow"></div>
+                <div class="algorithms-item-title">Calculadora de IMC</div>
+                <div class="algorithms-item-info">Calcula o Índice de Massa Corporal com base na altura e peso fornecidos.</div>
             </div>
         </a>
 
         <a href="pages/calculadora-de-media/index.php">
             <div class="algorithms-item">
-                <div class="notiglow"></div>
-                <div class="notiborderglow"></div>
-                <div class="notititle">Calculadora de Média</div>
-                <div class="notibody">Calcula a média aritmética simples de quatro notas informadas.</div>
+                <div class="algorithms-item-glow"></div>
+                <div class="algorithms-item-borderglow"></div>
+                <div class="algorithms-item-title">Calculadora de Média</div>
+                <div class="algorithms-item-info">Calcula a média aritmética simples de quatro notas informadas.</div>
             </div>
         </a>
 
 
         <a href="pages/calculadora/index.php">
             <div class="algorithms-item">
-                <div class="notiglow"></div>
-                <div class="notiborderglow"></div>
-                <div class="notititle">Calculadora Básica</div>
-                <div class="notibody">Realiza operações matemáticas básicas, incluindo soma, subtração, multiplicação e divisão.</div>
+                <div class="algorithms-item-glow"></div>
+                <div class="algorithms-item-borderglow"></div>
+                <div class="algorithms-item-title">Calculadora Básica</div>
+                <div class="algorithms-item-info">Realiza operações matemáticas básicas, incluindo soma, subtração, multiplicação e divisão.</div>
             </div>
         </a>
 
         <a href="pages/lanchonete/index.php">
             <div class="algorithms-item">
-                <div class="notiglow"></div>
-                <div class="notiborderglow"></div>
-                <div class="notititle">Lanchonete</div>
-                <div class="notibody">Sistema simples de pedidos que permite selecionar um lanche e definir a quantidade desejada.</div>
+                <div class="algorithms-item-glow"></div>
+                <div class="algorithms-item-borderglow"></div>
+                <div class="algorithms-item-title">Lanchonete</div>
+                <div class="algorithms-item-info">Sistema simples de pedidos que permite selecionar um lanche e definir a quantidade desejada.</div>
             </div>
         </a>
     </section>
