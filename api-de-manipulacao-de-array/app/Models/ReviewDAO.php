bb77fc1a837bd615e533137494d2358d59bba794
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-08-16 21:36:41 -0300

    feat: adds function updateOne

--- api-de-manipulacao-de-array/app/Models/ReviewDAO.php
+++ api-de-manipulacao-de-array/app/Models/ReviewDAO.php
@@ -58,5 +58,64 @@ class ReviewDAO extends Database {
 
         return $statement->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function updateOne($id, $data)
+    {
+        $placeInDatabase = $this->findOne($id);
+
+        $sql = "update reviews
+                        set 
+                            place_id=:place_id_value,
+                            name=:name_value,
+                            email=:email_value,
+                            stars=:stars_value,
+                            status=:status_value
+                    where id=:id_value
+             ";
+
+             $statement = ($this->getConnection())->prepare($sql);
+
+             $statement->bindValue(":id_value", $id);
+
+             $statement->bindValue(
+                 ":place_id_value",
+                 isset($data->place_id) ?
+                     $data->place_id :
+                     $placeInDatabase['place_id']
+             );
+
+             $statement->bindValue(
+                ":name_value",
+                isset($data->name) ?
+                    $data->name :
+                    $placeInDatabase['name']
+            );
+
+            $statement->bindValue(
+                ":email_value",
+                isset($data->email) ?
+                    $data->email :
+                    $placeInDatabase['email']
+            );
+
+            $statement->bindValue(
+                ":stars_value",
+                isset($data->stars) ?
+                    $data->stars :
+                    $placeInDatabase['stars']
+            );
+
+            $statement->bindValue(
+                ":status_value",
+                isset($data->status) ?
+                    $data->status :
+                    $placeInDatabase['status']
+            );
+
+
+            $statement->execute();
+
+            return ['success' => true];
+    }
 }
 ?>
